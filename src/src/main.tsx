9e96cadcfec93fcf76e053228eabcb22d9b57b98
ikabeshuka <ikabeshuka@gmail.com>
2026-07-22 21:42:09 +0300

    ללא אינטרנט

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import './fonts.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
